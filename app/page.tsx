6a10e66e0207a9cd8cce991c462191c78cab69d7
Vanessa Santana <santanavanessa91@gmail.com>
2025-07-17 22:04:12 -0300

    chore: adiciona o setup do Shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-red-500 bg-red-200 mb-5">Home page</h1>
+    <>
+      <h1 className="text-red-500 bg-red-200 mb-5">Home page</h1>
+      <Button>Clicar</Button>
+    </>
   );
 }
