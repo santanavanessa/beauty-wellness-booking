315026bcace36074199d5531a7a20c73b8e31256
Vanessa Santana <santanavanessa91@gmail.com>
2025-09-13 19:22:14 -0300

    feat: implementa componente navbar

---
+++ app/components/navbar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Calendar } from "lucide-react"
+import Link from "next/link"
+
+export function Navbar() {
+  return (
+    <div className="hidden md:flex">
+      <Button className="justify-start gap-2" variant="ghost" asChild>
+        <Link href="/bookings">
+          <Calendar size={18} />
+          Agendamentos
+        </Link>
+      </Button>
+    </div>
+  )
+}
